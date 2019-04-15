e33660d69a1f678961043785f2f97509a00173df
CptTony <zyc@nfls.rocks>
2019-04-15 04:40:21 -0500

    Added library

---
+++ decrypt-config/checksum.c
@@ -0,0 +1,27 @@
+/*
+ * This file is part of the Phicomm-k3 distribution (https://github.com/CptTZ/phicomm-k3-fun).
+ * Copyright (c) 2019 CptTonyZ.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <openssl/hmac.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
